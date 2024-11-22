1c3c5b6316a0482860122f50e59c8a3bf8e69e59
baba <babayoshihiko@mac.com>
2024-11-23 08:18:30 +0900

    Added PDF etc

---
+++ _install_packages.R
@@ -0,0 +1,42 @@
+
+
+install.packages("stargazer")
+install.packages("gt")
+
+install.packages("formattable")
+install.packages("reactablefmtr")
+
+## table
+install.packages("crosstable")
+install.packages("tables")
+install.packages("colorDF")
+install.packages("tableone")
+install.packages("rpivotTable")
+
+## ggplot2
+install.packages("ggplot2")
+install.packages("ggsci")
+install.packages("GGally")
+
+## Git
+install.packages("gitcreds")
+
+## PDF
+remotes::install_github('Gedevan-Aleksizde/rmdja@development', upgrade = "never")
+
+## Thesis
+install.packages("grid")
+install.packages("magick")
+install.packages("gridExtra")
+install.packages("RefManageR")
+
+## clinical
+install.packages("consort")
+install.packages("blockrand")
+install.packages("covadap")
+
+
+
+
+
+
